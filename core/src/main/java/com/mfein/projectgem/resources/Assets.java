4c5ee67abc384004fc143355d0318437cab7232d
Malcome Fein <mfein90@gmail.com>
2025-08-03 15:34:17 -0400

    add game assets

---
+++ core/src/main/java/com/mfein/projectgem/resources/Assets.java
@@ -0,0 +1,8 @@
+package com.mfein.projectgem.resources;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+public class Assets {
+    // asset manager
+    public final AssetManager manager = new AssetManager();
+}
